1a7b302857b36a1698ef769575b2446b73129d25
Karomy404 <91879977+Karomy404@users.noreply.github.com>
2021-10-25 12:57:29 +0200

    Update new-feed.py

--- new-feed.py
+++ new-feed.py
@@ -3,19 +3,19 @@
 import json
 
 
-print("nom du flux :")
+print("Nom du flux :")
 key = input()
 
-print("nom du webhook :")
+print("Nom du webhook :")
 username = input()
 
-print("url du flux rss :")
+print("URL du flux rss :")
 rss_url = input()
 
-print("url du webhook :")
+print("URL du webhook :")
 url_webhook = input()
 
-print("url avatar :")
+print("URL avatar :")
 url_avatar = input()
 
 guid ="/root/rss-discord/GUID/" + key + ".guid"
@@ -23,15 +23,18 @@ guid ="/root/rss-discord/GUID/" + key + ".guid"
 with open("/root/rss-discord/db.json", "r") as json_file:
     DICO = json.load(json_file)
 
+if key in DICO['URL'] :
+    print("ERREUR : Le nom du flux (key) existe deja dans le fichier db.json")
+else :
+    DICO["URL"][key] = rss_url
+    DICO["WEBHOOK"][key] = url_webhook
+    DICO["USERNAME"][key] = username
+    DICO["AVATAR"][key] = url_avatar
+    DICO["GUID"][key] = guid
 
-DICO["URL"][key] = rss_url
-DICO["WEBHOOK"][key] = url_webhook
-DICO["USERNAME"][key] = username
-DICO["AVATAR"][key] = url_avatar
-DICO["GUID"][key] = guid
+    print(json.dumps(DICO, sort_keys=True, indent=4))
+    print("SUCCESS : Le flux a bien été enregistré dans le fichier db.json")
 
-print(json.dumps(DICO, sort_keys=True, indent=4))
-
-with open('/root/rss-discord/db.json', 'w') as f:
-    f.write(json.dumps(DICO, sort_keys=True, indent=4))
-f.close()
+    with open('/root/rss-discord/db.json', 'w') as f:
+        f.write(json.dumps(DICO, sort_keys=True, indent=4))
+    f.close()
